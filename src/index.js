138c61d99d9b40f6d1d8b0cf41b6bf76d87be2be
prathamdubey <prathamd67@gmail.com>
2022-11-11 17:22:57 +0530

    Normalizer Added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { GlobalStyles } from "./globalStyles";
+import "normalize.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
