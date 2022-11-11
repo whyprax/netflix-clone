7ba700c6a6044eceaadd40a678d1c3876d5e667a
prathamdubey <prathamd67@gmail.com>
2022-11-11 20:50:02 +0530

    Footer Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { FooterContainer } from './containers/footer';
 import { JumbotronContainer } from './containers/jumbotron';
 
 export default function App() {
   return (
     <>
       <JumbotronContainer />
+      <FooterContainer />
     </>
   );
 }
--- src/containers/jumbotron.js
+++ src/containers/jumbotron.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import jumboData from '../fixtures/jumbo.json';
-import Jumbotron from '../components/jumbotron/index';
+import { Jumbotron } from '../components';
 
 export function JumbotronContainer() {
   return (
