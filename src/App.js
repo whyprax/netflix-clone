b5253a765f1d401fa580181105caf8d887831157
prathamdubey <prathamd67@gmail.com>
2022-11-11 17:20:05 +0530

    Jumbotron Updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
+import Jumbotron from "./components/jumbotron/index";
+import jumboData from "./fixtures/jumbo.json";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <h1>Hello world!</h1>
-    </div>
+    <>
+      <Jumbotron.Container>
+        {jumboData.map((item) => (
+          <Jumbotron key={item.id} direction={item.direction}>
+            <Jumbotron.Pane>
+              <Jumbotron.Title>{item.title}</Jumbotron.Title>
+            </Jumbotron.Pane>
+            <Jumbotron.Pane>
+              <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+              <Jumbotron.Image src={item.image} alt={item.alt} />
+            </Jumbotron.Pane>
+          </Jumbotron>
+        ))}
+      </Jumbotron.Container>
+    </>
   );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { GlobalStyles } from "./globalStyles";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <>
+    <GlobalStyles />
+    <App />
+  </>
+);
