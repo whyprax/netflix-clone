1a17d9361608187e9ef929f0faa5d2cd8334ec62
prathamdubey <prathamd67@gmail.com>
2022-12-13 06:33:06 +0530

    Browse:Header Added

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState, useContext } from "react";
 import { SelectProfileContainer } from "./profiles";
 import { FirebaseContext } from "../context/firebase";
-import { Loading } from "../components";
+import { Header, Loading } from "../components";
+import * as ROUTES from "../constants/routes";
+import logo from "../logo.svg";
 
 export function BrowseContainer({ slides }) {
   const { firebase } = useContext(FirebaseContext);
@@ -9,6 +11,8 @@ export function BrowseContainer({ slides }) {
 
   const [profile, setProfile] = useState({});
   const [loading, setLoading] = useState(true);
+  const [catergory, setCategory] = useState("series");
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     setTimeout(() => {
@@ -16,10 +20,64 @@ export function BrowseContainer({ slides }) {
     }, 5000);
   }, [profile.displayName]);
 
-  return (
+  return profile.displayName ? (
     <>
       {loading ? <Loading src={user.photoURL} /> : <Loading.ReleaseBody />}
-      <SelectProfileContainer user={user} setProfile={setProfile} />
+
+      <Header src="joker1" dontShowOnSmallViewPort>
+        <Header.Frame>
+          <Header.Group>
+            <Header.Logo to={ROUTES.HOME} src={logo} alt="Netflix" />
+            <Header.TextLink
+              active={catergory === "series" ? "true" : "false"}
+              onClick={() => setCategory("series")}
+            >
+              Series
+            </Header.TextLink>
+            <Header.TextLink
+              active={catergory === "films" ? "true" : "false"}
+              onClick={() => setCategory("films")}
+            >
+              Films
+            </Header.TextLink>
+          </Header.Group>
+
+          <Header.Group>
+            <Header.Search
+              searchTerm={searchTerm}
+              setSearchTerm={setSearchTerm}
+            />
+            <Header.Profile>
+              <Header.Picture src={user.photoURL} />
+              <Header.Dropdown>
+                <Header.Group>
+                  <Header.Picture src={user.photoURL} />
+                  <Header.TextLink>{user.displayName}</Header.TextLink>
+                </Header.Group>
+                <Header.Group>
+                  <Header.TextLink onClick={() => firebase.auth().signOut()}>
+                    Sign out
+                  </Header.TextLink>
+                </Header.Group>
+              </Header.Dropdown>
+            </Header.Profile>
+          </Header.Group>
+        </Header.Frame>
+
+        <Header.Feature>
+          <Header.FeatureCallOut>Watch Joker Now</Header.FeatureCallOut>
+          <Header.Text>
+            Forever alone in a crowd, failed comedian Arthur Fleck seeks
+            connection as he walks the streets of Gotham City. Arthur wears two
+            masks -- the one he paints for his day job as a clown, and the guise
+            he projects in a futile attempt to feel like he's part of the world
+            around him.
+          </Header.Text>
+          <Header.PlayButton>Play</Header.PlayButton>
+        </Header.Feature>
+      </Header>
     </>
+  ) : (
+    <SelectProfileContainer user={user} setProfile={setProfile} />
   );
 }
